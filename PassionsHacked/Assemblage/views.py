46724c2fa2cd20810a6ac8041e125aa4c14dd47f
niksmiljkovic <niksmiljkovic@gmail.com>
2016-10-14 16:41:43 +0200

    Adding group endpoints

--- PassionsHacked/Assemblage/views.py
+++ PassionsHacked/Assemblage/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.core import serializers
 import json
-import bookingapi
+from bookingapi import BookingAPI
+from models import *
 
 # Create your views here.
 
@@ -41,6 +43,51 @@ def signtest(request):
 		return HttpResponse("you are not signed in")
 	else:
 		return HttpResponse("Welcome " + request.user.username)
+
+def create_group(request):
+	try:
+		if request.user is None:
+			raise ValueError("Not logged in")
+ 
+		# create new group
+		name = request.GET['name']
+		destination = request.GET['destination']
+		from_date = request.GET['from_date']
+		to_date = request.GET['to_date']
+		group = Group(name=name, destination=destination, from_date=from_date, to_date=to_date)
+		group.save()
+
+		# add currently logged in user to the particiants list
+		group.participants.add(request.user)
+		group.save()
+
+		return HttpResponse(group.id)
+	except:
+		return HttpResponse("Error")
+
+def add_user_to_group(request):
+	try:
+		group_id = request.GET['group_id']
+		group = Group.objects.get(id = group_id)
+		group.participants.add(request.user)
+		return HttpResponse("OK")
+	except:
+		return HttpResponse("Error")
+
+def get_users_from_group(request):
+	#try:
+		group_id = request.GET['group_id']
+		group = Group.objects.get(id = group_id)
+		members = group.participants.all()
+		return HttpResponse(serializers.serialize('json', members))
+	#except:
+		return HttpResponse("Error")
+
+def add_block_to_group(request):
+	return HttpResponse("Not yet implemented")
+
+def get_blocks_from_group(request):
+	return HttpResponse("Not yet implemented")
 
 def get_countries(request):
 	api = bookingapi.BookingAPI()
