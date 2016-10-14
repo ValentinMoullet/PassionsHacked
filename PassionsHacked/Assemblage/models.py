59c4a7fa261c316cc83a89e242a3eb5b2aa054f8
Valentin Moullet <valentin@Valentins-MacBook-Pro.local>
2016-10-14 14:47:28 +0200

    Fix bug models

--- PassionsHacked/Assemblage/models.py
+++ PassionsHacked/Assemblage/models.py
@@ -11,15 +11,15 @@ class Group(models.Model):
 	creation_date = models.DateField(auto_now_add=True)
 	from_date = models.DateField()
 	to_date = models.DateField()
-	participants = models.ManyToMany(User)
+	participants = models.ManyToManyField(User)
 
 class Block(models.Model):
-	creating_user = models.ForeignKey(User)
+	creating_user = models.ForeignKey(User, related_name='%(class)s_requests_created')
 	bookingcom_block_id = models.CharField(max_length=100)
 	group = models.ForeignKey(Group)
-	voters = models.ManyToMany(User)
-	positive_vote = models.IntegerField()
-	negative_vote = models.IntegerField()
+	voters = models.ManyToManyField(User)
+	positive_votes = models.IntegerField()
+	negative_votes = models.IntegerField()
 
 
 
