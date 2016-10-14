f2fb0075497795f4ca5eba485fd3069aa052b097
Valentin Moullet <valentin@Valentins-MacBook-Pro.local>
2016-10-14 14:15:28 +0200

    Added some models

--- PassionsHacked/Assemblage/models.py
+++ PassionsHacked/Assemblage/models.py
@@ -1,9 +1,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Person(models.Model):
-	first_name = models.CharField(max_length=30)
-	last_name = models.CharField(max_length=30)
+class Group(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=100)
+	destination = models.CharField(max_length=100)
+	creation_date = models.DateField(auto_now_add=True)
+	
+
+class Profile(models.Model):
+	user = models.OneToOneField(User)
+	groups = models.ManyToMany(Group)
+	
+class Hotel:
+	id = models.AutoField(primary_key=True)
+	creating_user = models.ForeignKey(User)
+	
+
